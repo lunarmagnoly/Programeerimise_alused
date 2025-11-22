46f0ed2174cf537307e0cba69a48b0b407f2cc59
lunarmagnoly <mari.luna.tei@outlook.com>
2025-11-22 18:17:47 +0200

    Add second task draft solution

---
+++ 04_matemaatilised_avaldised/ex02_keerukamad_matemaatilised_avaldised/solution.py
@@ -0,0 +1,25 @@
+"""Math exercises vol2."""
+
+
+def time_converter(seconds: int) -> str:
+    """Convert time in seconds to hours and minutes."""
+    # Write your code here
+    return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
+
+
+def student_helper(angle: int) -> str:
+    """Return the sine and cosine of the given angle in degrees."""
+    # Write your code here
+    return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
+
+
+def greetings(n: int) -> str:
+    """Return a string that contains "Hey" n times."""
+    # Write your code here
+    return lots_of_heys
+
+
+def adding_numbers(num_a: int, num_b: int) -> str:
+    """Return given numbers added together as a string."""
+    # Write your code here
+    return string_numbers
