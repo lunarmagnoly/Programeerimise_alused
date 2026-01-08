f2c46ed4099815ab981461d1aaf59b5395493902
lunarmagnoly <mari.luna.tei@outlook.com>
2026-01-08 12:29:41 +0200

    Complete quadratic equation task solution

--- 07_tõeväärtused_ja_tingimuslaused/python_koolis/ex06.py
+++ 07_tõeväärtused_ja_tingimuslaused/python_koolis/ex06.py
@@ -12,16 +12,17 @@ from math import sqrt
 def solve_quadratic_equation(a: float, b: float, c: float):
     d = b**2 -(4 * a * c)
     if d == 0:
-        return - b / (2 * a)
+        return f"Ruutvõrrandi lahendus on {- b / (2 * a)}"
     elif d > 0:
         x1 = (- b + sqrt(d)) /(2 * a)
         x2 = (- b - sqrt(d)) /(2 * a)
-        return x1, x2
+        return f"Ruutvõrrandi lahendid on {x1} ja {x2}"
     else:
-        return "Lahendust ei ole"
+        return "Ruutvõrrandil ei ole lahendust"
 
 
 if __name__ == '__main__':
+    print("Ruutvõrrandi lahendamis programm: ")
     a = float(input("Sisestage a "))
     b = float(input("Sisestage b "))
     c = float(input("Sisestage c "))
