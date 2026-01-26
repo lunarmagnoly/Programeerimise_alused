89e99906236eb81e18d36ff0b6e21b278d8cb376
lunarmagnoly <mari.luna.tei@outlook.com>
2026-01-26 03:16:54 +0200

    Add string task solution.

---
+++ 10_sõne/ex01/solution.py
@@ -0,0 +1,19 @@
+
+
+first_name = 'James'
+last_name = 'Bond'
+
+full_name = f'{first_name} {last_name}'
+self_description_sentence = f'My name is {last_name}, {full_name}.'
+
+cake = "vahukoormarjadtäidispõhi"
+print(f"{cake[:8]}\n{cake[8:14]}\n{cake[14:20]}\n{cake[20:]}")
+
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[0:11][::-1]
+
+print(backwards)
+print(every_other)
+print(first_word_reversed)
