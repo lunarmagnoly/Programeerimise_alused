ee577f61fe2632a67253b30774f3bb845051b53d
lunarmagnoly <mari.luna.tei@outlook.com>
2026-02-03 04:13:38 +0200

    Add task description for speed and travel time

---
+++ 08_kordused/python_koolis_03/ex08.py
@@ -0,0 +1,14 @@
+"""
+Speed and Travel Time Task
+
+Increasing a car's speed reduces the time needed to reach a destination.
+But how much time can actually be saved?
+
+Write a program that:
+1. asks the user for the travel distance (in kilometers) at the start of the program;
+2. repeatedly asks the user for a driving speed (in km/h);
+3. calculates and prints:
+   - the travel time for the given speed;
+   - the difference in travel time compared to the previous speed;
+4. stops execution if the user presses Enter without entering a speed.
+"""
